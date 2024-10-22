dcdbe57ea370a098c5ca9cb68324fbf9d90bc9d2
Harvey <hjyemm@icloud.com>
2024-10-22 23:14:57 +0100

    Added More Request Information

--- Server/middleware/logging/requestLogger.ts
+++ Server/middleware/logging/requestLogger.ts
@@ -2,5 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 
 export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
   console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
+  if (Object.keys(req.query).length > 0) {
+    console.log('Query Parameters:', req.query); // Log the query parameters
+  }
+  if (Object.keys(req.body).length > 0) {
+    console.log('Request Body:', req.body); // Log the request body
+  }
   next();
 };
