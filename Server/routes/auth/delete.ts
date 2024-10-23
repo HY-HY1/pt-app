6edf074f8f610c9da09288d64ee6242a26f247b8
Harvey Yemm <hjyemm@icloud.com>
2024-10-23 09:41:51 +0100

    Task: Add Email Verification

---
+++ Server/routes/auth/delete.ts
@@ -0,0 +1,26 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { authenticateUser  } from '@middleware/index';
+import UserModel from '@models/userModel';
+import isExistingUser from '@utils/mongoose/isExistingUser';
+
+const router = Router();
+
+router.get('/', async (req: Request, res: Response, next: NextFunction) => { // Pass Email verification as middleware
+    try {
+        const user = (req as any).user
+
+        // Find Existing User To Avoid Errors
+
+        await isExistingUser(user.id)
+
+        // Add in email verification Function
+
+        await UserModel.findOneAndDelete({_id: user.id})
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+export default router;
+
