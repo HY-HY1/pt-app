4eaf438a6bee3d5e80b0a7c1c24402d70c31e71f
Harvey <hjyemm@icloud.com>
2024-10-23 17:08:46 +0100

    Added ID type checking

--- Server/utils/mongoose/isExistingUser.ts
+++ Server/utils/mongoose/isExistingUser.ts
@@ -1,16 +1,21 @@
-import UserModel from "@models/userModel";
+import mongoose from 'mongoose';  // Import mongoose for ObjectId type
+import UserModel from '@models/userModel';
 
-export default async function isExistingUser(_id:string) {
-    try {
-        const existingUser = await UserModel.findOne({_id: _id})
-
-        if(!existingUser) {
-            throw new Error('User was not found')
-        }
-
-        return existingUser
-    } catch (error) {
-        console.error(error)
-        return
+export default async function isExistingUser(_id: string) {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      throw new Error('Invalid user ID');
     }
+
+    const existingUser = await UserModel.findById(_id);  // Use findById for cleaner syntax
+
+    if (!existingUser) {
+      throw new Error('User not found');
+    }
+
+    return existingUser;
+  } catch (error) {
+    console.error(error);
+    return null;  // Return null explicitly if there's an error
+  }
 }
