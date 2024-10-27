402c984d83169593c080e052fc97932dad1b88da
Harvey <hjyemm@icloud.com>
2024-10-27 09:30:57 +0000

    Vercel Deployment Changes

--- Server/Server.ts
+++ Server/Server.ts
@@ -30,9 +30,9 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 export default app
